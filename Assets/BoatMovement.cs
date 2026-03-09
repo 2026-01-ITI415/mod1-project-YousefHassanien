2c0f266ff19817702603b616ceb6c134ff78d74e
YousefHassanien <79435259+YousefHassanien@users.noreply.github.com>
2026-03-08 23:14:18 -0400

    Finish

--- Assets/BoatMovement.cs
+++ Assets/BoatMovement.cs
@@ -6,6 +6,9 @@ public class BoatMovement : MonoBehaviour
     public float forwardSpeed = 500f; 
     public float dodgeSpeed = 300f;   
     private Rigidbody rb;
+    
+    // A boolean to check if the game is over so we stop adding force
+    private bool isFinished = false; 
 
     void Start()
     {
@@ -14,20 +17,37 @@ public class BoatMovement : MonoBehaviour
 
     void FixedUpdate() 
     {
-        // 1. Constant forward drift
-        rb.AddForce(Vector3.forward * forwardSpeed * Time.deltaTime);
+        // Only allow movement if the game is NOT finished
+        if (!isFinished) 
+        {
+            rb.AddForce(Vector3.forward * forwardSpeed * Time.deltaTime);
 
-        // 2. Lateral dodging
-        float horizontalInput = Input.GetAxis("Horizontal"); 
-        rb.AddForce(Vector3.right * horizontalInput * dodgeSpeed * Time.deltaTime);
+            float horizontalInput = Input.GetAxis("Horizontal"); 
+            rb.AddForce(Vector3.right * horizontalInput * dodgeSpeed * Time.deltaTime);
+        }
     }
 
-    void OnTriggerEnter(Collider other)
+void OnTriggerEnter(Collider other)
     {
-        // Restart if we hit a storm drain
         if (other.CompareTag("Drain"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            RestartGame();
         }
+        else if (other.CompareTag("Finish"))
+        {
+            // 1. The boat crossed the finish line!
+            isFinished = true; 
+
+            // 2. Kill all built-up momentum instantly by turning on isKinematic
+            rb.isKinematic = true; 
+
+            // 3. Wait 3 seconds, then restart
+            Invoke("RestartGame", 3f); 
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
